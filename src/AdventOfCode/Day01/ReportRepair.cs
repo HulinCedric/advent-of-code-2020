a742d8ba10e5021ce1fbe9ec7c9ca4fccbdd27e1
Cédric Hulin <hulin.cedric@gmail.com>
2021-01-04 09:03:34 +0100

    refactor: Linq query syntax for Day 01

--- src/AdventOfCode/Day01/ReportRepair.cs
+++ src/AdventOfCode/Day01/ReportRepair.cs
@@ -12,26 +12,16 @@ namespace AdventOfCode.Day01
             (from first in expenseReport
              from second in expenseReport
              from third in expenseReport
-             select new
-             {
-                 sum = first + second + third,
-                 product = first * second * third
-             })
-            .Where(result => result.sum == TargetedSum)
-            .Select(result => result.product)
+             where first + second + third == TargetedSum
+             select first * second * third)
             .First();
 
         public static int GetFixedExpenseReportValueWithTwoEntries(IEnumerable<int> expenseReport)
                     =>
             (from first in expenseReport
              from second in expenseReport
-             select new
-             {
-                 sum = first + second,
-                 product = first * second
-             })
-             .Where(result => result.sum == TargetedSum)
-             .Select(result => result.product)
-             .First();
+             where first + second == TargetedSum
+             select first * second)
+            .First();
     }
 }
