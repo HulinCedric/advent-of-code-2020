a439cf6751e2dff7fdec9b4ef07ede89e2f344a3
Cédric Hulin <hulin.cedric@gmail.com>
2021-01-22 14:00:23 +0100

    refactor: Use input file data attribute for Day 05

--- test/AdventOfCode.Tests/Day05/BoardingPassScannerShould.cs
+++ test/AdventOfCode.Tests/Day05/BoardingPassScannerShould.cs
@@ -6,24 +6,11 @@ namespace AdventOfCode.Day05.Tests
     {
         [Theory]
         [InlineData("F\nB", 1)]
+        [InputFileData("Day05/input.txt", 832)]
         public void Get_highest_seat_id(
             string boardingPassesDescription,
             int expectedHighestSeatId)
         {
-            //When
-            var highestSeatId = BoardingPassScanner.GetHighestSeatId(boardingPassesDescription);
-
-            //Then
-            Assert.Equal(expectedHighestSeatId, highestSeatId);
-        }
-
-        [Fact]
-        public void Get_highest_seat_id_for_problem()
-        {
-            //Given
-            var boardingPassesDescription = BoardingPassDescription.ProblemDescription;
-            var expectedHighestSeatId = 832;
-
             //When
             var highestSeatId = BoardingPassScanner.GetHighestSeatId(boardingPassesDescription);
 
