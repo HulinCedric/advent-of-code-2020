043ea36d6ec6e2e69b6845f9800d82ec9ad1d606
Cédric Hulin <hulin.cedric@gmail.com>
2021-01-22 23:28:25 +0100

    refactor: 2020 - Day 05 - Input file

--- test/AdventOfCode.Tests/Day05/BoardingPassFinderShould.cs
+++ test/AdventOfCode.Tests/Day05/BoardingPassFinderShould.cs
@@ -7,24 +7,11 @@ namespace AdventOfCode.Day05.Tests
         [Theory]
         [InlineData("FF\nBF", 1)]
         [InlineData("BFF\nFBF", 3)]
+        [InputFileData("Day05/input.txt", 517)]
         public void Find_the_only_missing_seat_id_in_full_flight_boarding_passes(
              string boardingPassesDescription,
              int expectedAvailableSeatId)
         {
-            //When
-            var availableSeatId = BoardingPassFinder.FindAvailableSeatId(boardingPassesDescription);
-
-            //Then
-            Assert.Equal(expectedAvailableSeatId, availableSeatId);
-        }
-
-        [Fact]
-        public void Find_the_only_missing_seat_id_in_full_flight_boarding_passes_for_problem()
-        {
-            //Given
-            var boardingPassesDescription = BoardingPassDescription.ProblemDescription;
-            var expectedAvailableSeatId = 517;
-
             //When
             var availableSeatId = BoardingPassFinder.FindAvailableSeatId(boardingPassesDescription);
 
