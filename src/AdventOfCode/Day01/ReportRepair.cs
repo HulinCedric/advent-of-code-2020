efec643e4b56f3401fc41e58cd69e1e8421438dd
Cédric Hulin <hulin.cedric@gmail.com>
2021-01-22 23:14:31 +0100

    refactor: 2020 - Day 01 - Input file

--- src/AdventOfCode/Day01/ReportRepair.cs
+++ src/AdventOfCode/Day01/ReportRepair.cs
@@ -7,21 +7,26 @@ namespace AdventOfCode.Day01
     {
         private const int TargetedSum = 2020;
 
-        public static int GetFixedExpenseReportValueWithThreeEntries(List<int> expenseReport)
+        public static int GetFixedExpenseReportValueWithThreeEntries(string expenseReportDescription) 
             =>
-            (from first in expenseReport
-             from second in expenseReport
-             from third in expenseReport
+            (from first in ParseExpenseReport(expenseReportDescription)
+             from second in ParseExpenseReport(expenseReportDescription)
+             from third in ParseExpenseReport(expenseReportDescription)
              where first + second + third == TargetedSum
              select first * second * third)
             .First();
-
-        public static int GetFixedExpenseReportValueWithTwoEntries(IEnumerable<int> expenseReport)
-                    =>
-            (from first in expenseReport
-             from second in expenseReport
+        
+        public static int GetFixedExpenseReportValueWithTwoEntries(string expenseReportDescription) 
+            => 
+            (from first in ParseExpenseReport(expenseReportDescription)
+             from second in ParseExpenseReport(expenseReportDescription)
              where first + second == TargetedSum
              select first * second)
             .First();
+
+        private static IEnumerable<int> ParseExpenseReport(string expenseReportDescription)
+            => expenseReportDescription
+            .Split("\n")
+            .Select(expenseReportEntryDescription => int.Parse(expenseReportEntryDescription));
     }
 }
---
+++ test/AdventOfCode.Tests/Day01/ReportRepairShould.cs
@@ -0,0 +1,35 @@
+using Xunit;
+
+namespace AdventOfCode.Day01.Tests
+{
+    public class ReportRepairShould
+    {
+        [Theory]
+        [InlineData("1721\n979\n366\n299\n675\n1456", 514579)]
+        [InputFileData("Day01/input.txt", 787776)]
+        public void Fix_expense_report_with_two_entries(
+            string expenseReportDescription,
+            int expectedFixedExpenseReportValue)
+        {
+            //When
+            var fixedExpenseReportValue = ReportRepair.GetFixedExpenseReportValueWithTwoEntries(expenseReportDescription);
+
+            //Then
+            Assert.Equal(expectedFixedExpenseReportValue, fixedExpenseReportValue);
+        }
+
+        [Theory]
+        [InlineData("1721\n979\n366\n299\n675\n1456", 241861950)]
+        [InputFileData("Day01/input.txt", 262738554)]
+        public void Fix_expense_report_with_three_entries(
+            string expenseReportDescription,
+            int expectedFixedExpenseReportValue)
+        {
+            //When
+            var fixedExpenseReportValue = ReportRepair.GetFixedExpenseReportValueWithThreeEntries(expenseReportDescription);
+
+            //Then
+            Assert.Equal(expectedFixedExpenseReportValue, fixedExpenseReportValue);
+        }
+    }
+}
