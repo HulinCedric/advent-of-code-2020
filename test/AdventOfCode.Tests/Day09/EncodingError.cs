0491df4b97b0a7d3b7f61aab29d76a8e16ddb596
Cédric Hulin <hulin.cedric@gmail.com>
2021-03-18 22:11:15 +0100

    feat: 2020 - Day 09 - Part One - Raw solution

--- test/AdventOfCode.Tests/Day09/EncodingError.cs
+++ test/AdventOfCode.Tests/Day09/EncodingError.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Xunit;
 
 namespace AdventOfCode.Day09
@@ -6,20 +7,36 @@ namespace AdventOfCode.Day09
     {
         [Theory]
         [InlineData(PortOutputs.Example, 5, 127)]
-        // [InputFileData("Day09/input.txt", 25, 1548)]
+        [InputFileData("Day09/input.txt", 25, 27911108)]
         public void Find_the_first_number_that_is_not_the_sum_of_two_preamble_numbers(
             string portOutputs,
             int preambleLength,
-            int expectedIntruderNumber)
+            long expectedIntruderNumber)
         {
             // Given
-           
+            var numbers = portOutputs.Split("\n").Select(long.Parse).ToList();
+            var index = 0;
+            long currentNumber;
 
             // When
-          
+            do
+            {
+                var preamble = numbers.Skip(index).Take(preambleLength).ToList();
+
+                var validNumbers =
+                    from x in preamble
+                    from y in preamble
+                    select x + y;
+
+                currentNumber = numbers.ElementAt(preambleLength + index);
+
+                var isInvalid = !validNumbers.Contains(currentNumber);
+                if (isInvalid) 
+                    break;
+            } while (preambleLength + ++index < numbers.Count);
 
             // Then
-            Assert.Equal(expectedIntruderNumber, expectedIntruderNumber);
+            Assert.Equal(expectedIntruderNumber, currentNumber);
         }
     }
 }
