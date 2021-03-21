d6d5bde992d672bc6a0cc83ce3072064750e7cc4
Cédric Hulin <hulin.cedric@gmail.com>
2021-03-21 23:09:41 +0100

    feat: 2020 - Day 09 - Part Two - Raw solution

--- test/AdventOfCode.Tests/Day09/EncodingError.cs
+++ test/AdventOfCode.Tests/Day09/EncodingError.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -7,7 +8,7 @@ namespace AdventOfCode.Day09
     {
         [Theory]
         [InlineData(PortOutputs.Example, 5, 127)]
-        [InputFileData("Day09/input.txt", 25, 27911108)]
+        [InputFileData("Day09/input.txt", 25, 27_911_108L)]
         public void Find_the_first_number_that_is_not_the_sum_of_two_preamble_numbers(
             string portOutputs,
             int preambleLength,
@@ -41,7 +42,7 @@ namespace AdventOfCode.Day09
 
         [Theory]
         [InlineData(PortOutputs.Example, 127L, 15L, 47L)]
-        // [InputFileData("Day09/input.txt", 27911108L, ?, ?)]
+        [InputFileData("Day09/input.txt", 27_911_108L, 1_178_062L, 2_845_692L)]
         public void Find_a_contiguous_set_of_at_least_two_numbers_which_sum_to_the_invalid_number(
             string portOutputs,
             long invalidNumber,
@@ -50,15 +51,40 @@ namespace AdventOfCode.Day09
         {
             // Given
             var numbers = portOutputs.Split("\n").Select(long.Parse).ToList();
-            var actualSmallestWeaknessNumber = 15L;
-            var actualLargestWeaknessNumber = 47L;
+            var contiguousNumberSearchIndex = 0;
+            IEnumerable<long> contiguousNumbers;
 
             // When
+            do
+            {
+                contiguousNumbers = GetContiguousNumbersOrEmpty(
+                    invalidNumber,
+                    contiguousNumberSearchIndex++,
+                    numbers);
+            } while (!contiguousNumbers.Any());
 
+            var actualSmallestWeaknessNumber = contiguousNumbers.Min();
+            var actualLargestWeaknessNumber = contiguousNumbers.Max();
 
             // Then
             Assert.Equal(expectedSmallestWeaknessNumber, actualSmallestWeaknessNumber);
             Assert.Equal(expectedLargestWeaknessNumber, actualLargestWeaknessNumber);
+        }
+
+        private static IEnumerable<long> GetContiguousNumbersOrEmpty(
+            long numberToFind,
+            int startIndex,
+            List<long> numbers)
+        {
+            var sum = 0L;
+            var contiguousNumbers = numbers
+                .Skip(startIndex)
+                .TakeWhile(number => (sum += number) <= numberToFind)
+                .ToList();
+
+            return contiguousNumbers.Sum() == numberToFind ?
+                contiguousNumbers :
+                Enumerable.Empty<long>();
         }
     }
 }
