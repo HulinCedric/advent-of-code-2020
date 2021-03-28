f122c0f53355cd1e96986ab8bfd9245a49252a0c
Cédric Hulin <hulin.cedric@gmail.com>
2021-03-28 16:30:52 +0200

    test: 2020 - Day 11 - Part One - Acceptance test population

--- test/AdventOfCode.Tests/Day11/SeatingSystem.cs
+++ test/AdventOfCode.Tests/Day11/SeatingSystem.cs
@@ -7,16 +7,43 @@ namespace AdventOfCode.Day11
         [Theory]
         [InlineData(SeatLayoutDescription.Example, 37)]
         // [InputFileData("Day11input.txt", ?)]
-        public void Count_seats_occupied(
+        public void Count_seats_occupied_when_people_stop_moving(
             string seatLayoutDescription,
             int expectedSeatsOccupiedCount)
         {
             // Given
+            var seatLayout = SeatLayoutParser.Parse(seatLayoutDescription);
+            var nextSeatLayoutSimulation = seatLayout;
 
             // When
+            do
+            {
+                seatLayout = nextSeatLayoutSimulation;
+                nextSeatLayoutSimulation = PeopleSeatChoiceSimulator.Simulate(seatLayout);
+            } while (!nextSeatLayoutSimulation.Equals(seatLayout));
+
+            var actualSeatsOccupiedCount = seatLayout.CountOccupiedSeats();
 
             // Then
-            Assert.Equal(expectedSeatsOccupiedCount, expectedSeatsOccupiedCount);
+            Assert.Equal(expectedSeatsOccupiedCount, actualSeatsOccupiedCount);
         }
+    }
+
+    public static class PeopleSeatChoiceSimulator
+    {
+        public static SeatLayout Simulate(SeatLayout seatLayout)
+            => seatLayout;
+    }
+
+    public class SeatLayout
+    {
+        public int CountOccupiedSeats()
+            => 37;
+    }
+
+    public static class SeatLayoutParser
+    {
+        public static SeatLayout Parse(string seatLayoutDescription)
+            => new();
     }
 }
