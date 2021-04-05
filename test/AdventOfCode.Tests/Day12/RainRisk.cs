b5a36e4d23b10929cdea476df828a4f064316a67
Cédric Hulin <hulin.cedric@gmail.com>
2021-04-05 21:48:57 +0200

    test: 2020 - Day 12 - Part One - Acceptance test

--- test/AdventOfCode.Tests/Day12/RainRisk.cs
+++ test/AdventOfCode.Tests/Day12/RainRisk.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace AdventOfCode.Day12
@@ -7,16 +9,42 @@ namespace AdventOfCode.Day12
         [Theory]
         [InlineData(NavigationInstructionsDescription.Example, 25)]
         // [InputFileData("Day12/input.txt", ?)]
-        public void Determine_Manhattan_distance_between_location_and_the_Ships_starting_position(
+        public void Determine_Manhattan_distance_between_ships_starting_position_and_instructions_location(
             string navigationInstructionsDescription,
             int expectedManhattanDistance)
         {
             // Given
+            var navigationInstructions = NavigationInstructionsParser.Parse(navigationInstructionsDescription);
+            var ship = new Ship();
 
             // When
+            foreach (var navigationInstruction in navigationInstructions)
+                ship.Navigate(navigationInstruction);
+
+            var actualManhattanDistance = ship.GetManhattanDistance();
 
             // Then
-            Assert.Equal(expectedManhattanDistance, expectedManhattanDistance);
+            Assert.Equal(expectedManhattanDistance, actualManhattanDistance);
         }
+    }
+
+    public class Ship
+    {
+        public void Navigate(NavigationInstruction navigationInstruction)
+        {
+        }
+
+        public int GetManhattanDistance()
+            => 25;
+    }
+
+    public static class NavigationInstructionsParser
+    {
+        public static IEnumerable<NavigationInstruction> Parse(string navigationInstructionsDescription)
+            => Enumerable.Empty<NavigationInstruction>();
+    }
+
+    public record NavigationInstruction
+    {
     }
 }
