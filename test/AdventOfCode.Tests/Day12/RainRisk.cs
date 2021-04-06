a18c3ee5971f2e75d44d702b0af37ba22c7b6666
Cédric Hulin <hulin.cedric@gmail.com>
2021-04-06 22:00:06 +0200

    feat: Navigation instructions parser

--- test/AdventOfCode.Tests/Day12/RainRisk.cs
+++ test/AdventOfCode.Tests/Day12/RainRisk.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode.Day12
     {
         [Theory]
         [InlineData(NavigationInstructionsDescription.Example, 25)]
-        // [InputFileData("Day12/input.txt", ?)]
+        [InputFileData("Day12/input.txt", 25)]
         public void Determine_Manhattan_distance_between_ships_starting_position_and_instructions_location(
             string navigationInstructionsDescription,
             int expectedManhattanDistance)
@@ -41,10 +41,30 @@ namespace AdventOfCode.Day12
     public static class NavigationInstructionsParser
     {
         public static IEnumerable<NavigationInstruction> Parse(string navigationInstructionsDescription)
-            => Enumerable.Empty<NavigationInstruction>();
+            => navigationInstructionsDescription
+                .Split("\n")
+                .Select(
+                    navigationInstructionDescription =>
+                        new NavigationInstruction(
+                            ExtractAction(navigationInstructionDescription),
+                            ExtractValue(navigationInstructionDescription)));
+
+        private static int ExtractValue(string navigationInstructionDescription)
+            => int.Parse(navigationInstructionDescription[1..]);
+
+        private static char ExtractAction(string navigationInstructionDescription)
+            => navigationInstructionDescription[0];
     }
 
     public record NavigationInstruction
     {
+        public NavigationInstruction(char action, int value)
+        {
+            Action = action;
+            Value = value;
+        }
+
+        public char Action { get; }
+        public int Value { get; }
     }
 }
