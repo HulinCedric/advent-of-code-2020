2f3e661552906c54aa0f6cfe48a22f600f5cf3fa
Cédric Hulin <hulin.cedric@gmail.com>
2021-04-10 17:45:39 +0200

    feat: Navigation instruction execution

--- test/AdventOfCode.Tests/Day12/RainRisk.cs
+++ test/AdventOfCode.Tests/Day12/RainRisk.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode.Day12
     {
         [Theory]
         [InlineData(NavigationInstructionsDescription.Example, 25)]
-        [InputFileData("Day12/input.txt", 25)]
+        [InputFileData("Day12/input.txt", 1133)]
         public void Determine_Manhattan_distance_between_ships_starting_position_and_instructions_location(
             string navigationInstructionsDescription,
             int expectedManhattanDistance)
@@ -32,22 +32,70 @@ namespace AdventOfCode.Day12
     public class Ship
     {
         private Direction direction;
-        private int lattitude;
-        private int longitude;
-        
+        private int x;
+        private int y;
+
         public Ship()
         {
             direction = Direction.East;
-            longitude = 0;
-            lattitude = 0;
+            y = 0;
+            x = 0;
         }
 
         public void Navigate(NavigationInstruction navigationInstruction)
-        {
-        }
+            => navigationInstruction.Execute(this);
 
         public int GetManhattanDistance()
-            => 25;
+            => Math.Abs(x) + Math.Abs(y);
+
+        public void MoveForward(int unit)
+            => MoveTo(direction, unit);
+
+        public void MoveTo(Direction instructionDirection, int unit)
+        {
+            switch (instructionDirection)
+            {
+                case Direction.North:
+                    x += unit;
+                    break;
+                case Direction.East:
+                    y += unit;
+                    break;
+                case Direction.South:
+                    x -= unit;
+                    break;
+                case Direction.West:
+                    y -= unit;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void TurnRight(int degree)
+            => direction = degree switch
+            {
+                90 when direction == Direction.East => Direction.South,
+                180 when direction == Direction.East => Direction.West,
+                270 when direction == Direction.East => Direction.North,
+
+                90 when direction == Direction.South => Direction.West,
+                180 when direction == Direction.South => Direction.North,
+                270 when direction == Direction.South => Direction.East,
+
+                90 when direction == Direction.West => Direction.North,
+                180 when direction == Direction.West => Direction.East,
+                270 when direction == Direction.West => Direction.South,
+
+                90 when direction == Direction.North => Direction.East,
+                180 when direction == Direction.North => Direction.South,
+                270 when direction == Direction.North => Direction.West,
+
+                _ => direction
+            };
+
+        public void TurnLeft(int degree)
+            => TurnRight(360 - degree);
     }
 
     public enum Direction
@@ -61,13 +109,15 @@ namespace AdventOfCode.Day12
     public static class NavigationInstructionsParser
     {
         public static IEnumerable<NavigationInstruction> Parse(string navigationInstructionsDescription)
-            => navigationInstructionsDescription
+        {
+            return navigationInstructionsDescription
                 .Split("\n")
                 .Select(
                     navigationInstructionDescription =>
                         NavigationInstruction.CreateInstance(
                             ExtractAction(navigationInstructionDescription),
                             ExtractValue(navigationInstructionDescription)));
+        }
 
         private static int ExtractValue(string navigationInstructionDescription)
             => int.Parse(navigationInstructionDescription[1..]);
@@ -79,7 +129,8 @@ namespace AdventOfCode.Day12
     public abstract record NavigationInstruction(char Action, int Value)
     {
         public static NavigationInstruction CreateInstance(char action, int value)
-            => action switch
+        {
+            return action switch
             {
                 'N' => new NorthNavigationInstruction(action, value),
                 'S' => new SouthNavigationInstruction(action, value),
@@ -90,24 +141,33 @@ namespace AdventOfCode.Day12
                 'F' => new ForwardNavigationInstruction(action, value),
                 _ => throw new ArgumentOutOfRangeException(nameof(action), action, null)
             };
+        }
+
+        public abstract void Execute(Ship ship);
     }
 
     public record ForwardNavigationInstruction
         : NavigationInstruction
     {
-        public ForwardNavigationInstruction(char Action, int Value)
-            : base(Action, Value)
+        public ForwardNavigationInstruction(char action, int value)
+            : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.MoveForward(Value);
     }
 
     public record RightNavigationInstruction
         : NavigationInstruction
     {
-        public RightNavigationInstruction(char Action, int Value)
-            : base(Action, Value)
+        public RightNavigationInstruction(char action, int value)
+            : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.TurnRight(Value);
     }
 
     public record LeftNavigationInstruction
@@ -117,6 +177,9 @@ namespace AdventOfCode.Day12
             : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.TurnLeft(Value);
     }
 
     public record WestNavigationInstruction
@@ -126,6 +189,9 @@ namespace AdventOfCode.Day12
             : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.MoveTo(Direction.West, Value);
     }
 
     public record EastNavigationInstruction
@@ -136,6 +202,9 @@ namespace AdventOfCode.Day12
 
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.MoveTo(Direction.East, Value);
     }
 
     public record NorthNavigationInstruction
@@ -145,6 +214,9 @@ namespace AdventOfCode.Day12
             : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.MoveTo(Direction.North, Value);
     }
 
     public record SouthNavigationInstruction
@@ -154,5 +226,8 @@ namespace AdventOfCode.Day12
             : base(action, value)
         {
         }
+
+        public override void Execute(Ship ship)
+            => ship.MoveTo(Direction.South, Value);
     }
 }
