e1b44cd9ae7d8e5da52249e742d641e9befb2ced
Cédric Hulin <hulin.cedric@gmail.com>
2021-04-26 09:17:41 +0200

    chore: 2020 - Day 14 - Part One - Acceptance test

---
+++ test/AdventOfCode.Tests/Day14/DockingData.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace AdventOfCode.Day14
+{
+    public class DockingData
+    {
+        [Theory]
+        [InlineData(InitializationProgramDescription.Example, 165)]
+        // [InputFileData("Day14/input.txt", ?)]
+        public void Determine_the_sum_of_all_values_left_in_memory_after_it_completes(
+            string initializationProgramDescription,
+            int expectedSum)
+        {
+            // Given
+
+            // When
+          
+            // Then
+            Assert.Equal(expectedSum, expectedSum);
+        }
+    }
+}
---
+++ test/AdventOfCode.Tests/Day14/InitializationProgramDescription.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode.Day14
+{
+    public static class InitializationProgramDescription
+    {
+        public const string Example =
+            "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n" +
+            "mem[8] = 11\n" +
+            "mem[7] = 101\n" +
+            "mem[8] = 0";
+    }
+}
