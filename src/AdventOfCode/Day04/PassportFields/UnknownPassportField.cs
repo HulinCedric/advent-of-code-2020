86cb7459fed04ba303181f339a51960ea4409039
Cédric Hulin <hulin.cedric@gmail.com>
2021-05-09 10:13:55 +0200

    refactor: Unused parameter

--- src/AdventOfCode/Day04/PassportFields/UnknownPassportField.cs
+++ src/AdventOfCode/Day04/PassportFields/UnknownPassportField.cs
@@ -3,9 +3,11 @@
     public class UnknownPassportField
         : PassportField
     {
+        private readonly string name;
+
         internal UnknownPassportField(string name, string value)
             : base(value)
-        { }
+            => this.name = name;
 
         public override bool IsValid()
             => false;
