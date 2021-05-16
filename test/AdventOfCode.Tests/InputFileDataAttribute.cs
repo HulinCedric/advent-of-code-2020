c0034165f351a922795fb6739750c9acccb85711
Cédric Hulin <hulin.cedric@gmail.com>
2021-05-16 13:47:57 +0200

    refactor: Loop can be converted into LINQ-expression

--- test/AdventOfCode.Tests/InputFileDataAttribute.cs
+++ test/AdventOfCode.Tests/InputFileDataAttribute.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode
         private readonly object[] theoryData;
 
         /// <summary>
-        /// Load data from a input file as the data source for a theory.
+        ///     Load data from a input file as the data source for a theory.
         /// </summary>
         /// <param name="filePath">The absolute or relative path to the input file to load.</param>
         /// <param name="theoryData">The theory data.</param>
@@ -24,41 +24,41 @@ namespace AdventOfCode
             this.theoryData = theoryData;
         }
 
-        /// <inheritDoc />
-        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
-        {
-            if (testMethod == null) { throw new ArgumentNullException(nameof(testMethod)); }
-
-            // Get the absolute path to the input file
-            var path = Path.IsPathRooted(filePath)
-                ? filePath
-                : Path.GetRelativePath(Directory.GetCurrentDirectory(), filePath);
-
-            if (!File.Exists(path))
-            {
-                throw new ArgumentException($"Could not find file at path: {path}");
-            }
-
-            var theoryDataWithFileContrat = new List<object> { GetFileContent(filePath) };
-            foreach (var parameter in theoryData)
-            {
-                theoryDataWithFileContrat.Add(parameter);
-            }
-
-            yield return theoryDataWithFileContrat.ToArray();
-        }
-
         /// <summary>
-        /// Gets the content of the file.
+        ///     Gets the content of the file.
         /// </summary>
         /// <param name="filePath">The file path.</param>
         /// <returns>The file content.</returns>
         /// <remarks>
-        /// All lines in file are separated by a LF (Line Feed) for normalization purpose.
+        ///     All lines in file are separated by a LF (Line Feed) for normalization purpose.
         /// </remarks>
         private static string GetFileContent(string filePath)
             => string.Join(
                 "\n",
                 File.ReadAllLines(filePath));
+
+        /// <inheritDoc />
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (testMethod == null)
+                throw new ArgumentNullException(nameof(testMethod));
+
+            // Get the absolute path to the input file
+            var path = Path.IsPathRooted(filePath) ?
+                filePath :
+                Path.GetRelativePath(Directory.GetCurrentDirectory(), filePath);
+
+            if (!File.Exists(path))
+                throw new ArgumentException($"Could not find file at path: {path}");
+
+            var theoryDataWithFileContent = new List<object>
+            {
+                GetFileContent(filePath)
+            };
+
+            theoryDataWithFileContent.AddRange(theoryData);
+
+            yield return theoryDataWithFileContent.ToArray();
+        }
     }
 }
